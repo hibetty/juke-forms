899e4afe0092377825f2082524ecf07d0f20bfb8
Ketevann <ketevan.tsin@gmail.com>
2017-05-17 17:39:12 -0400

    created filtered arists

---
+++ browser/react/components/FilterableArtistsContainer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import FilterInput from './FilterInput';
+import Artists from './Artists';
+
+
+export default class FilterableArtistsContainer extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {inputValue: ""}
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange (evt) {
+    const value = evt.target.value;
+    this.setState({
+    inputValue: value
+    });
+  }
+  render(){
+    const inputValue = this.state.inputValue;
+    const filteredArtists = this.props.artists.filter(artist =>
+       artist.name.match(inputValue));
+    return (
+      <div>
+        <FilterInput handleChange={this.handleChange} />
+        <Artists artists={filteredArtists} />
+      </div>);
+  }
+}
+
